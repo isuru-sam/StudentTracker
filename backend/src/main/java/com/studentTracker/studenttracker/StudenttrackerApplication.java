38428a7311b8c8f84ce3be701c871305f5a3f06f
Isuru Samaraweera <iisuru@gmail.com>
2017-07-24 15:37:16 +0530

    Initial set up

---
+++ backend/src/main/java/com/studentTracker/studenttracker/StudenttrackerApplication.java
@@ -0,0 +1,12 @@
+package com.studentTracker.studenttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudenttrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudenttrackerApplication.class, args);
+	}
+}

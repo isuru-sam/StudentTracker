0a0ca59e1b356861d82ffa709f2905b667c641bf
Isuru Samaraweera <iisuru@gmail.com>
2017-07-26 16:57:38 +0530

    dbscript

--- backend/src/main/resources/dbscript.sql
+++ backend/src/main/resources/dbscript.sql
@@ -1,2 +1,6 @@
 create table student(  
-id bigint not null primary key,name varchar(100),address varchar(200),is_full_time boolean ,version int)
+id bigint not null primary key,name varchar(100),address varchar(200),is_full_time boolean ,version int);
+
+insert into student values (9,'john','london',true,1);
+
+insert into student values (10,'sam' ,'colombo',false,1)
